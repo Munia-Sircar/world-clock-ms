5d1ed5f0d1c2d0b190c8ecda1cfbb13975141a54
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-09-20 11:10:18 -0500

    added functionality in the select with multiple locations

--- index.js
+++ index.js
@@ -32,3 +32,23 @@ function displayCurrentTime() {
 
 displayCurrentTime();
 setInterval(displayCurrentTime, 1000);
+
+function showSelectedCity(event) {
+  let city = event.target.value;
+  let cityTimeZone = moment().tz(city);
+  let cityName = city.split("/")[1];
+
+  let chosenCity = document.querySelector("#cities");
+  chosenCity.innerHTML = `<div class="city" id="">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTimeZone.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTimeZone.format(
+    "h:mm:ss"
+  )}<small> ${cityTimeZone.format("A")}</small></div>
+  </div>`;
+}
+
+let selectCity = document.querySelector("#choose-city");
+selectCity.addEventListener("change", showSelectedCity);
